6394145c06c2abfcd0ed8f8916708b5de65a7d7f
Akinrinade Samson <harkinsam247@gmail.com>
2024-11-15 11:35:38 +0100

    Added Task controller

--- user-service/src/main/java/app/enforcatechnologies/user_service/controller/TaskController.java
+++ user-service/src/main/java/app/enforcatechnologies/user_service/controller/TaskController.java
@@ -1,2 +1,38 @@
-package app.enforcatechnologies.user_service.controller;public class TaskController {
+package app.enforcatechnologies.user_service.controller;
+
+import app.enforcatechnologies.user_service.model.Task;
+import app.enforcatechnologies.user_service.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/add/{jobTalentId}")
+    public ResponseEntity<Task> addTask(@RequestBody Task task, @PathVariable Long jobTalentId) {
+        Task newTask = taskService.addTaskToJobTalent(task, jobTalentId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
+    }
+
+    // Get all tasks
+    @GetMapping("/all")
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    // Get tasks by JobTalent ID
+    @GetMapping("/jobtalent/{jobTalentId}")
+    public ResponseEntity<List<Task>> getTasksByJobTalent(@PathVariable Long jobTalentId) {
+        List<Task> tasks = taskService.getTasksByJobTalent(jobTalentId);
+        return ResponseEntity.ok(tasks);
+    }
 }
