a21c9639ae6e06cb72f3e23c2b3368e97e2c60b9
Akinrinade Samson <harkinsam247@gmail.com>
2024-11-15 11:37:05 +0100

    Added Task service

--- user-service/src/main/java/app/enforcatechnologies/user_service/repository/TaskRepository.java
+++ user-service/src/main/java/app/enforcatechnologies/user_service/repository/TaskRepository.java
@@ -1,2 +1,10 @@
-package app.enforcatechnologies.user_service.repository;public interface TaskRepository {
+package app.enforcatechnologies.user_service.repository;
+
+import app.enforcatechnologies.user_service.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByJobTalentId(Long jobTalentId);
 }
